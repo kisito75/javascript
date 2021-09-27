cc2182a9cb98f806967eea9e59d59d7d8929a1a3
kisito75 <89266163+kisito75@users.noreply.github.com>
2021-09-27 19:29:16 +0200

    Add files via upload

---
+++ javascript/Atelier.js
@@ -0,0 +1,96 @@
+	/*alert("Bonjour gainde web dans un autre fichier");
+
+
+var prenom = "Christian Gennigon"
+console.log('Christian Gennigon')
+
+
+
+var nom = "Bampoky"
+console.log('Bampoky')
+
+
+/*for (var i=1; i=5; i<=10; i++);
+{
+	alert(i);
+}*/
+
+
+
+/*var nombre = 10;
+do{
+	alert(nombre);
+	nombre--;
+}
+	while (nombre>-1)*/
+
+
+
+	/*var age = prompt ("saisir votre nom");
+	alert (age);
+
+
+
+	if (age>0 && age<=18) 
+	{
+		alert(" vous etes adolescent")
+	}
+
+
+
+
+
+	if (age>18 && age<=40) 
+	{
+		alert(" vous etes jeune")
+	}
+
+
+
+
+
+
+	if (age>40 && age<=300) 
+	{
+		alert(" vous etes vieux")
+	}*/
+
+
+
+	/*var prenom = prompt('saisir votre prenom')
+	alert('prenom')
+
+
+
+	var nom = prompt('saisir votre nom')
+	alert('nom')
+
+
+
+	var age = prompt('saisir votre age')
+	alert('age')*/
+
+
+
+
+
+
+	var prenom = prompt ("saisir votre prenom");
+	
+
+
+	var nom = prompt ("saisir votre nom");
+	
+
+
+    var age = prompt ("saisir votre age");
+
+
+
+	alert ("votre prenom est "+prenom+", votre nom est "+nom+", votre age est "+age+" ");
+
+
+	
+
+
+
